fc4aa36a17b3607a9edac34b26d64c0d02ed9b32
git4lhd2017 <number@l@163.com>
2020-01-16 01:06:10 +0800

    springMVC的配置还有一个bug，可能是因为mvc的问题

--- src/main/java/com/transitory/reception/controller/UserController.java
+++ src/main/java/com/transitory/reception/controller/UserController.java
@@ -1,12 +1,26 @@
 package com.transitory.reception.controller;
 
+import com.transitory.reception.beans.Manager;
+import com.transitory.reception.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("findAll")
+    public List<Manager> findAll(){
+        return userService.findAll();
+    }
 
 
 
